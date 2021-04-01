786c8dc32bc979db695d76670eaaafc4985acf3a
Hadenix <vsegda_online_007@bk.ru>
2021-04-01 13:42:46 +0300

    A5_Pro: Add Engineering Mode
    * Engineering Mode Compiled from Mtk Alps

--- Android.mk
+++ Android.mk
@@ -6,4 +6,16 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),A5_Pro)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := EngineerMode
+LOCAL_MODULE_OWNER := umidigi
+LOCAL_SRC_FILES := proprietary/priv-app/EngineerMode/EngineerMode.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_PRIVILEGED_MODULE := true
+include $(BUILD_PREBUILT)
+
 endif
