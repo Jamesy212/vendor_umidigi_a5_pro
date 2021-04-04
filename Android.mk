d33507d6508f1385cefb863a5026764ca40051a5
Hadenix <vsegda_online_007@bk.ru>
2021-04-04 16:13:40 +0300

    A5_Pro: Update proprietary blobs
    * Copy libmtk-ril.so to vendor_overlay
    * Add more ims permissions

--- Android.mk
+++ Android.mk
@@ -6,16 +6,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter A5_Pro,$(TARGET_DEVICE)),)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := ImsService
-LOCAL_MODULE_OWNER := umidigi
-LOCAL_SRC_FILES := proprietary/priv-app/ImsService/ImsService.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
-
 endif
